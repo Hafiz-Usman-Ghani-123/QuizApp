20a83a735ab78671b064766fe7a9fd53914f7cf4
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2024-07-08 23:12:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,31 @@ signBtn.addEventListener("click", () => {
 //
 // Coming code for signup FORM submission
 //
+document.getElementById("signupF").addEventListener("submit", function (e) {
+  e.preventDefault(); // Default form submission behavior ko rokna
+  const email = document.getElementById("email1").value; // Username ko read karna
+  const password = document.getElementById("pass1").value; // Password ko read karna
+
+  // Naya user object
+  const newUser = {
+    email: email,
+    password: password,
+  };
+
+  // tamam users ki aik array
+  let users = [];
+  //
+  const userExists = users.some((user) => user.email === username);
+
+  if (userExists) {
+    alert("Username already exists! Please choose another username.");
+  } else {
+    // Naya user ko users array me add karna
+    users.push(newUser);
+
+    // Updated users array ko `localStorage` me save karna
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert("Signup successful!");
+  }
+});
