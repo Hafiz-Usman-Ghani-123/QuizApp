b0fcce179d866fae9f050b5847e9bb592e2c5a2a
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2024-07-10 20:58:58 +0500

    options p work continue

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ logBtn.addEventListener("click", () => {
 });
 // for changing login  ==>  sign up form
 signBtn.addEventListener("click", () => {
-  if (signForm.style.display === "block"){ 
+  if (signForm.style.display === "block") {
     alert(`Are you a Blind ? 
-That's already selected !`)
-  }else{
+That's already selected !`);
+  } else {
     logForm.style.display = "none";
     signForm.style.display = "block";
   }
@@ -46,7 +46,7 @@ document.getElementById("signupF").addEventListener("submit", function (e) {
 });
 
 // Login Form Submission
-// 
+//
 document.getElementById("loginF").addEventListener("submit", function (e) {
   e.preventDefault(); // Default form submission behavior ko rokna
   const email = document.getElementById("email2").value; // Username ko read karna
@@ -62,12 +62,86 @@ document.getElementById("loginF").addEventListener("submit", function (e) {
 
   if (validUser) {
     // window.open("https://google.com"); // <- This is what makes it open in a new window.;
-    document.querySelector(".container1").style.display = 'none'
+    document.querySelector(".container1").style.display = "none";
     // console.log(bothForms);
-    
+
     alert("Login successful !"); // Success message show karna
   } else {
     alert("Invalid username or password"); // Error message show karna
   }
 });
-// 
+// ----------------------------------
+//          QUIZ APPLICATION
+// ----------------------------------
+const questions = [
+  {
+    ques: `Which of the following is correct about features of JavaScript?`,
+    a: `JavaScript is complementary to and integrated with HTML.`,
+    b: `JavaScript is open and cross-platform.`,
+    c: `Both of the above.`,
+    d: `All of the above.`,
+    correct: this.c,
+  },
+  {
+    ques: `Which of the following is the correct syntax to print a page using JavaScript?`,
+    a: `document.print();`,
+    b: `browser.print();.`,
+    c: `navigator.print();`,
+    d: `window.print();`,
+    correct: this.d,
+  },
+  {
+    ques: `Which built-in method returns the character at the specified index?`,
+    a: `characterAt()`,
+    b: `getCharAt()`,
+    c: `charAt()`,
+    d: `None of the above.`,
+    correct: this.c,
+  },
+  {
+    ques: `Which built-in method returns the characters in a string beginning at the specified location?`,
+    a: `substr()`,
+    b: `getSubstring()`,
+    c: `slice()`,
+    d: `None of the above.`,
+    correct: this.a,
+  },
+  {
+    ques: `Which of the following function of Boolean object returns a string containing the source of the Boolean object?`,
+    a: `valueOf()`,
+    b: `toSource()`,
+    c: `toString()`,
+    d: `None of the above.`,
+    correct: this.b,
+  },
+];
+
+// ye code sirf questions print karny k liye hy
+let num = 0;
+let questionBox = document.getElementById("question");
+
+questionBox.innerHTML = questions[num].ques;
+
+document.getElementById("nextQuesBtn").addEventListener("click", () => {
+  // questions ki taadaad se zada na bary
+  if (num === questions.length - 1) {
+    num = 0;
+  } else {
+    num++;
+  }
+  questionBox.innerText = questions[num].ques;
+});
+
+// ye code sirf options print karny k liye hy
+let op1 = document.getElementById("option1").textContent;
+let op2 = document.getElementById("option2").textContent;
+let op3 = document.getElementById("option3").textContent;
+let op4 = document.getElementById("option4").textContent;
+
+
+document.getElementById("nextQuesBtn").addEventListener("click", () => {
+  op1 = questions[num].a;
+  op2 = questions[num].b;
+  op3 = questions[num].c;
+  op4 = questions[num].d;
+});
