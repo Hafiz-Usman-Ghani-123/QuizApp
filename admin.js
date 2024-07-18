deabb726b6919cd26b2cbf11d91374411696d41d
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2024-07-18 21:02:00 +0500

    after completing questions ?

--- admin.js
+++ admin.js
@@ -4,13 +4,15 @@ btn.addEventListener("click", () => {
   const op2 = document.getElementById("op2");
   const op3 = document.getElementById("op3");
   const op4 = document.getElementById("op4");
+  const correct = document.getElementById("correct");
   const ques = document.getElementById("question");
   if (
     ques.value == "" ||
     op1.value == "" ||
     op2.value == "" ||
     op3.value == "" ||
-    op4.value == ""
+    op4.value == "" ||
+    correct.value == ""
   ) {
     alert("Please Fill All Inputs !!!");
   } else {
@@ -18,10 +20,11 @@ btn.addEventListener("click", () => {
     let prevQues = questions ? JSON.parse(questions) : [];
     const fulQues = {
       ques: ques.value,
-      op1: op1.value,
-      op2: op2.value,
-      op3: op3.value,
-      op4: op4.value,
+      a: op1.value,
+      b: op2.value,
+      c: op3.value,
+      d: op4.value,
+      correct: correct.value,
     };
     // // written question ko previous question ki array k sath kar diya
     prevQues?.push(fulQues);
--- quiz.js
+++ quiz.js
@@ -1,50 +1,50 @@
 // ----------------------------------
 //          QUIZ APPLICATION
 // ----------------------------------
-const questions = [
-  {
-    ques: `Which of the following is correct about features of JavaScript?`,
-    a: `JavaScript is complementary to and integrated with HTML.`,
-    b: `JavaScript is open and cross-platform.`,
-    c: `Both of the above.`,
-    d: `All of the above.`,
-    correct: "c",
-  },
-  {
-    ques: `Which of the following is the correct syntax to print a page using JavaScript?`,
-    a: `document.print();`,
-    b: `browser.print();.`,
-    c: `navigator.print();`,
-    d: `window.print();`,
-    correct: "d",
-  },
-  {
-    ques: `Which built-in method returns the character at the specified index?`,
-    a: `characterAt()`,
-    b: `getCharAt()`,
-    c: `charAt()`,
-    d: `None of the above.`,
-    correct: "c",
-  },
-  {
-    ques: `Which built-in method returns the characters in a string beginning at the specified location?`,
-    a: `substr()`,
-    b: `getSubstring()`,
-    c: `slice()`,
-    d: `None of the above.`,
-    correct: "a",
-  },
-  {
-    ques: `Which of the following function of Boolean object returns a string containing the source of the Boolean object?`,
-    a: `valueOf()`,
-    b: `toSource()`,
-    c: `toString()`,
-    d: `None of the above.`,
-    correct: "b",
-  },
-];
-//          (structure)
-
+// const questions = [
+//   {
+//     ques: `Which of the following is correct about features of JavaScript?`,
+//     a: `JavaScript is complementary to and integrated with HTML.`,
+//     b: `JavaScript is open and cross-platform.`,
+//     c: `Both of the above.`,
+//     d: `All of the above.`,
+//     correct: "c",
+//   },
+//   {
+//     ques: `Which of the following is the correct syntax to print a page using JavaScript?`,
+//     a: `document.print();`,
+//     b: `browser.print();.`,
+//     c: `navigator.print();`,
+//     d: `window.print();`,
+//     correct: "d",
+//   },
+//   {
+//     ques: `Which built-in method returns the character at the specified index?`,
+//     a: `characterAt()`,
+//     b: `getCharAt()`,
+//     c: `charAt()`,
+//     d: `None of the above.`,
+//     correct: "c",
+//   },
+//   {
+//     ques: `Which built-in method returns the characters in a string beginning at the specified location?`,
+//     a: `substr()`,
+//     b: `getSubstring()`,
+//     c: `slice()`,
+//     d: `None of the above.`,
+//     correct: "a",
+//   },
+//   {
+//     ques: `Which of the following function of Boolean object returns a string containing the source of the Boolean object?`,
+//     a: `valueOf()`,
+//     b: `toSource()`,
+//     c: `toString()`,
+//     d: `None of the above.`,
+//     correct: "b",
+//   },
+// ];
+const questions = JSON.parse(localStorage.getItem("Question"));
+let quesLen = questions.length;
 let num = 0; // question or option k index k liye ...
 let selected = false; // to check the radio btn is checked or not ..
 let correcOrNot = document.getElementById("correcOrNot"); // sahi galat print krna
@@ -95,7 +95,7 @@ btn.addEventListener("click", () => {
     }
   }
   if (selected) {
-    if (num == 4) {
+    if (num === quesLen) {
       setTimeout(() => {
         alert(`${correct} Correct Answer
 ${incorrect} Incorrect Answer `);
